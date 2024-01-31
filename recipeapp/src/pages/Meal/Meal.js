a188395e716fce7215a07e9a76160f4a5b459cc0
Your Name <you@example.com>
2024-01-31 10:50:09 +0300

    starting

---
+++ recipeapp/src/pages/Meal/Meal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const Meal = () => {
+  return (
+    <SafeAreaView>
+      <Text>Meal</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Meal;
