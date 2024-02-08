478f1c28c5e2895cb679d7320d3b07b2b3a5d9e5
Your Name <you@example.com>
2024-02-08 11:55:51 +0300

    mealcardstyle

--- recipeapp/src/components/MealCard/MealCard.style.js
+++ recipeapp/src/components/MealCard/MealCard.style.js
@@ -5,14 +5,23 @@ const deviceSize = Dimensions.get('window');
 export default StyleSheet.create({
   container: {
     flexDirection: 'col',
+    margin: 5,
   },
   image: {
     width: deviceSize.width,
     height: 150,
     resizeMode: 'cover',
+    borderRadius: 15,
+    overflow: 'hidden',
+    justifyContent: 'flex-end',
   },
   text: {
-    color: 'black',
+    color: 'white',
     fontSize: 20,
+    fontWeight: 'bold',
+    lineHeight: 15,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    width: deviceSize.width,
+    textAlign: 'right',
   },
 });
